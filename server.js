8b2b7b3f0582077ba32522eac7f1d758b75bc238
bobbyy16 <bobbyyyyy16@gmail.com>
2025-10-27 12:42:09 +0530

    feat: add initial server setup with dotenv and webhook routes

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv");
+const webhookRoutes = require("./routes/webhookRoutes");
 
-const PORT = process.env.PORT || 3000;
+dotenv.config();
+
+const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send(`
+            <html>
+              <head><title>Success!</title></head>
+              <body style="text-align: center; font-family: Arial, sans-serif; margin-top: 50px;">
+                <h1>You did it!</h1>
+                <img src="https://media.giphy.com/media/XreQmk7ETCak0/giphy.gif" alt="Cool kid doing thumbs up" />
+              </body>
+            </html>
+          `);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});
+app.use("/webhook", webhookRoutes);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on port http://localhost:${PORT}`)
+);
